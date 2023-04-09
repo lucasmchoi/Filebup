ffdc97da5de6ae3329d1ae0e06610d4d0cf4a6d7
Luca Choi <73029406+lucasmchoi@users.noreply.github.com>
2023-04-09 20:38:07 +0200

    :ambulance: Fix env vars

--- src/dockerids_helper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# get all docker container ids and number
-dockerids=$(curl -s -XGET --unix-socket /var/run/docker.sock http://localhost/containers/json | jq -r '.[].Id')
-dockernum=$(echo "$dockerids" | wc -l)
-ownshortid=$HOSTNAME

4a2a4b2150f4f629e6bb93528dc8b98c6a92c5d9
Luca Choi <73029406+lucasmchoi@users.noreply.github.com>
2023-06-18 20:34:57 +0200

    change structure and functions

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM alpine:3.17
-RUN apk add --no-cache bash curl python3 py3-pip borgbackup openssh jq
+RUN apk add --no-cache bash curl wget python3 py3-pip borgbackup borgmatic openssh jq bind-tools
 RUN pip3 install pyyaml
 COPY src /filebup
 RUN mkdir /filebup/config
 RUN mkdir /filebup/volumes
-RUN mkdir /filebup/cronhelpers
 RUN mkdir -p /root/.ssh/
 RUN chmod +x /filebup/entrypoint.sh
 ENTRYPOINT ["/filebup/entrypoint.sh"] 
--- src/entrypoint_helper.py
+++ src/entrypoint_helper.py
@@ -6,105 +6,19 @@ import subprocess
 with open('/filebup/config/config.yaml') as configf:
     configl = yaml.load(configf, Loader=yaml.SafeLoader)
 
-# iterate over all repos in config.yaml
-for volume in configl.keys():
-    if configl[volume]['service'] == 'borg':
-        localpath = configl[volume]['localpath']
-        borguser = configl[volume]['borguser']
-        borghost = configl[volume]['borghost']
-        borgport = configl[volume]['borgport']
-        borgdir = configl[volume]['borgdir']
-        borgsshkey = configl[volume]['borgsshkey']
-        borgpass = configl[volume]['borgpass']
-
-        borgschedule = configl[volume]['borgschedule']
-        hcheckurlbup = configl[volume]['hcheckurlbup']
-
-        pruneschedule = configl[volume]['pruneschedule']
-        hcheckurlprune = configl[volume]['hcheckurlprune']
-        hcheckurlcompact = configl[volume]['hcheckurlcompact']
-
-        checkschedule = configl[volume]['checkschedule']
-        hcheckurlcheck = configl[volume]['hcheckurlcheck']
+# iterate over config.yaml
+for crons in configl.keys():
+    if configl[crons]['service'] == 'borg':
+        schedule = configl[crons]['schedule']
+        command = configl[crons]['command']
 
         # load cronfile
         with open('/var/spool/cron/crontabs/root') as cronf:
             cronc = cronf.read()
-        
+
         # check if cron for regular pulling exists alreay in cronfile
-        if volume in cronc:
-            print('Volume {} backup with borg already in cronfile'.format(volume))
+        if command in cronc:
+            print('Command {} already in cronfile'.format(command))
         else:
-            helpercreatesh = "#!/bin/bash\n"
-            helpercreatesh += "chmod 400 /filebup/config/secrets/{}\n".format(borgsshkey)
-            helpercreatesh += "healthcheckurl={}\n".format(hcheckurlbup)
-            helpercreatesh += "export BORG_RSH='ssh -o StrictHostKeyChecking=no -i /filebup/config/secrets/{}'\n".format(borgsshkey)
-            helpercreatesh += "export BORG_PASSPHRASE='{}'\n".format(borgpass)
-            helpercreatesh += "REPOSITORY='ssh://{}@{}:{}/./{}'\n".format(borguser, borghost, borgport, borgdir)
-            helpercreatesh += "curl -fsS -m 10 --retry 5 -o /dev/null $healthcheckurl/start\n"
-            helpercreatesh += "dockerids=$(curl -s -XGET --unix-socket /var/run/docker.sock http://localhost/containers/json | jq -r '.[].Id')\n"
-            helpercreatesh += "ownshortid=$HOSTNAME\n"
-            helpercreatesh += "for id in $dockerids\n"
-            helpercreatesh += "do\n"
-            helpercreatesh += "[[ $id = $ownshortid* ]] || curl -s -XPOST --unix-socket /var/run/docker.sock -H 'Content-Type: application/json' http://localhost/containers/$id/stop\n"
-            helpercreatesh += "done\n"
-            helpercreatesh += "m=$(borg create -s --show-version --show-rc -c 300 -C auto,lzma,6 $REPOSITORY::$(date +'%Y%m%d-%H%M') /filebup/volumes/{} 2>&1)\n".format(localpath)
-            helpercreatesh += "for id in $dockerids\n"
-            helpercreatesh += "do\n"
-            helpercreatesh += "[[ $id = $ownshortid* ]] || curl -s -XPOST --unix-socket /var/run/docker.sock -H 'Content-Type: application/json' http://localhost/containers/$id/start\n"
-            helpercreatesh += "done\n"
-            helpercreatesh += "unset dockerids ownshortid currentnum\n"
-            helpercreatesh += "curl -fsS -m 10 --retry 5 --data-raw \"$(echo \"$m\" | tail -c 100000)\" $healthcheckurl/$?\n"
-            helpercreatesh += "unset healthcheckurl BORG_RSH BORG_PASSPHRASE REPOSITORY m"
-
-            with open('/filebup/cronhelpers/{}-create.sh'.format(volume), 'w') as writer:
-                writer.write(helpercreatesh)
-
-            subprocess.run(['echo ''"''{} sh /filebup/cronhelpers/{}-create.sh''"'' >> /var/spool/cron/crontabs/root'.format(borgschedule, volume)], shell=True) 
-
-            print('Volume {} backup create with borg written to cronfile'.format(volume))
-
-
-            helperpruneesh = "#!/bin/bash\n"
-            helperpruneesh += "chmod 400 /filebup/config/secrets/{}\n".format(borgsshkey)
-            helperpruneesh += "healthcheckurl={}\n".format(hcheckurlprune)
-            helperpruneesh += "healthcheckurlcompact={}\n".format(hcheckurlcompact)
-            helperpruneesh += "export BORG_RSH='ssh -o StrictHostKeyChecking=no -i /filebup/config/secrets/{}'\n".format(borgsshkey)
-            helperpruneesh += "export BORG_PASSPHRASE='{}'\n".format(borgpass)
-            helperpruneesh += "REPOSITORY='ssh://{}@{}:{}/./{}'\n".format(borguser, borghost, borgport, borgdir)
-            helperpruneesh += "curl -fsS -m 10 --retry 5 -o /dev/null $healthcheckurl/start\n"
-            helperpruneesh += "m=$(borg prune -v --list --keep-within=1w -d=28 -w=12 -m=12 -y=5 $REPOSITORY 2>&1)\n"
-            helperpruneesh += "curl -fsS -m 10 --retry 5 --data-raw \"$(echo \"$m\" | tail -c 100000)\" $healthcheckurl/$?\n"
-            helperpruneesh += "unset m\n"
-            helperpruneesh += "curl -fsS -m 10 --retry 5 -o /dev/null $healthcheckurlcompact/start\n"
-            helperpruneesh += "m=$(borg compact -v $REPOSITORY 2>&1)\n"
-            helperpruneesh += "curl -fsS -m 10 --retry 5 --data-raw \"$(echo \"$m\" | tail -c 100000)\" $healthcheckurlcompact/$?\n"
-            helperpruneesh += "unset healthcheckurl healthcheckurlcompact BORG_RSH BORG_PASSPHRASE REPOSITORY m"
-
-            with open('/filebup/cronhelpers/{}-prune.sh'.format(volume), 'w') as writer:
-                writer.write(helperpruneesh)
-
-            subprocess.run(['echo ''"''{} sh /filebup/cronhelpers/{}-prune.sh''"'' >> /var/spool/cron/crontabs/root'.format(pruneschedule, volume)], shell=True) 
-
-            print('Volume {} backup prune and compact with borg written to cronfile'.format(volume))
-
-
-            helperchecksh = "#!/bin/bash\n"
-            helperchecksh += "chmod 400 /filebup/config/secrets/{}\n".format(borgsshkey)
-            helperchecksh += "healthcheckurl={}\n".format(hcheckurlcheck)
-            helperchecksh += "export BORG_RSH='ssh -o StrictHostKeyChecking=no -i /filebup/config/secrets/{}'\n".format(borgsshkey)
-            helperchecksh += "export BORG_PASSPHRASE='{}'\n".format(borgpass)
-            helperchecksh += "REPOSITORY='ssh://{}@{}:{}/./{}'\n".format(borguser, borghost, borgport, borgdir)
-            helperchecksh += "curl -fsS -m 10 --retry 5 -o /dev/null $healthcheckurl/start\n"
-            helperchecksh += "m=$(borg check -v $REPOSITORY 2>&1)\n"
-            helperchecksh += "curl -fsS -m 10 --retry 5 --data-raw \"$(echo \"$m\" | tail -c 100000)\" $healthcheckurl/$?\n"
-            helperchecksh += "unset healthcheckurl BORG_RSH BORG_PASSPHRASE REPOSITORY m"
-
-            with open('/filebup/cronhelpers/{}-check.sh'.format(volume), 'w') as writer:
-                writer.write(helperchecksh)
-
-            subprocess.run(['echo ''"''{} sh /filebup/cronhelpers/{}-check.sh''"'' >> /var/spool/cron/crontabs/root'.format(checkschedule, volume)], shell=True) 
-
-            print('Volume {} backup check with borg written to cronfile'.format(volume))
-    else:
-        print('{} not yet supported'.format(configl[volume]['service']))
+            subprocess.run(['echo ''"''{} {}''"'' >> /var/spool/cron/crontabs/root'.format(schedule, command)], shell=True) 
+            print('Command {} written to cronfile'.format(command))
